3d725cc3959806afe87fd0c9276be694453b0bc6
phyauab <phyauab@connect.ust.hk>
2022-03-31 10:48:48 +0800

    update config

--- backend/src/db/sequelize.js
+++ backend/src/db/sequelize.js
@@ -1,7 +1,11 @@
 const { Sequelize } = require("sequelize");
+const DATABASE = process.env.DATABASE;
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+const HOST = process.env.HOST;
 
-const sequelize = new Sequelize("foodie", "root", "admin", {
-  host: "localhost",
+const sequelize = new Sequelize(DATABASE, USERNAME, PASSWORD, {
+  host: HOST,
   dialect: "mysql",
 });
 
